52ac24570b00476bc6461bc4cab754cf07586281
CocoLaNoix <corentin.duc@live.fr>
2015-12-23 11:38:09 +0100

    Create bases.py

---
+++ bases.py
@@ -0,0 +1,10 @@
+def plateau(n):
+    liste = [[0]*n for i in range (n)]
+    return liste
+
+def affichagel(liste):
+    for i in range (0,n):
+        print (liste[i], "\n")
+
+n = eval(input("Entrer un entier pair : "))
+affichagel(plateau(n))
