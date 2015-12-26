688b99e82787940ee5429f6a0bd190a85df8cf54
CocoLaNoix <corentin.duc@live.fr>
2015-12-26 10:45:30 +0100

    Update alignements.py

--- alignements.py
+++ alignements.py
@@ -1,4 +1,5 @@
 def ligne(liste,n,j):
+    """ne fonctionne que pour aligner 5 pièces"""
     for i in range(n-j):
         for x in range(n+1):
                 if liste[x][i] == liste[x][i+1] == liste[x][i+2] == liste[x][i+3] == liste[x][i+4]:
