0337661fcdcd5e1aa689e3b23e696779d8c0a163
CocoLaNoix <corentin.duc@live.fr>
2015-12-30 10:44:19 +0100

    Update alignements.py

--- alignements.py
+++ alignements.py
@@ -1,26 +1,25 @@
 """ne fonctionne que pour aligner 5 pièces"""
 
 def ligne(liste,n,j):
-    for i in range(n-j):
-        for x in range(n+1):
-                if liste[x][i] == liste[x][i+1] == liste[x][i+2] == liste[x][i+3] == liste[x][i+4]:
-                    return True
+    for i in range(n-j+1):
+        for x in range(n):
+            if liste[x][i] == liste[x][i+1] == liste[x][i+2] == liste[x][i+3] == liste[x][i+4]:
+                return True
 
 def colonne(liste,n,j):
-   for i in range (n+1):
-       for x in range (n-j):
+   for i in range (n):
+       for x in range (n-j+1):
            if liste[x][i] == liste[x+1][i] == liste[x+2][i] == liste[x+3] == liste[x+4][i]:
                return True
-             
-"""j'ai pas vérifié si les diagonales marchent"""
+
 def diagonalegauche(liste,n,j):
-    for i in range (n-j):
-        for x in range (n-j):
+    for i in range (n-j+1):
+        for x in range (n-j+1):
             if liste[x][i] == liste[x+1][i+1] == liste[x+2][i+2] == liste[x+3][i+3] == liste[x+4][i+4]:
                 return True
 
 def diagonaledroite(liste,n,j):
-    for i in range (-1,-(n-j)):
-        for x in range (n-j):
+    for i in range (-1,-(n-j+1)):
+        for x in range (n-j+1):
             if liste[x][i] == liste[x+1][i-1] == liste[x+2][i-2] == liste[x+3][i-3] == liste[x+4][i-4]:
                 return True
